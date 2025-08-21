c11a71a048e45767e61a54d0935c7cd46c951667
longlong0210 <thanhlong0210.jvb@gmail.com>
2025-08-21 09:07:10 +0700

    refresh

--- Thực tập/Week1/Test.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyABLonsDEQ7veJFWZf6lLlHvtPw9K4lBMs")
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Kể tên 3 con vật"
-)
-print(response.text)
